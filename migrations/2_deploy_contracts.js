255304bc151eecfe8aa60e149cf830b3286ce036
ufogreys <heronnseibel@gmail.com>
2023-04-19 23:32:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+const Pool = artifacts.require("Pool");
+const PToken = artifacts.require("PToken");
+const Bridge = artifacts.require("Bridge");
+const CallProxy = artifacts.require("CallProxy");
+const Wrapper = artifacts.require("Wrapper");
+
+module.exports = function (deployer, network, accounts) {
+
+}
